87470787c925b90aa7acfda4d22b1b4ecf6d92c3
Rostislav Korst <60556749+RostislavKorst@users.noreply.github.com>
2022-01-21 17:30:41 +0300

    Add files via upload

---
+++ models/schemas.py
@@ -0,0 +1,38 @@
+from typing import List
+
+from pydantic import BaseModel
+
+
+class TaskBase(BaseModel):
+    text: str
+
+
+class Task(TaskBase):
+    id: str
+    user_id: str
+
+    class Config:
+        orm_mode = True
+
+
+class TaskCreate(TaskBase):
+    pass
+
+
+class UserBase(BaseModel):
+    username: str
+    email: str
+    name: str
+    hashed_password: str
+
+
+class User(UserBase):
+    id: str
+    tasks: List[Task] = []
+
+    class Config:
+        orm_mode = True
+
+
+class UserCreate(UserBase):
+    pass
